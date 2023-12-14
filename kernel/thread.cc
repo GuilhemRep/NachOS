d20a44a0664b1f525628eb891b44b790576cda4b
chanattan <zentsugo@gmail.com>
2023-12-14 11:34:15 +0100

    fixed hello?

--- kernel/thread.cc
+++ kernel/thread.cc
@@ -126,7 +126,7 @@ int Thread::Start(Process *owner,
 	// Initialize thread content.
 	// Allocate the user stack.
 	int sp = process->addrspace->StackAllocate();
-	InitThreadContext(0, sp, arg);
+	InitThreadContext(sp, sp, arg);
 
 	// Allocate the stack for the RISC-V simulator.
 	int8_t* sim_sp = AllocBoundedArray(SIMULATORSTACKSIZE);
